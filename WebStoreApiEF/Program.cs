607141bb2d87f000f2e2d30bf804517aef29b669
Ngabgna <Ngabgna@BLACKJUKER>
2023-10-06 09:39:57 +0200

    Ajoutez des fichiers projet.

---
+++ WebStoreApiEF/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using WebStoreApiEF.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/O enAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    string connexionString = builder.Configuration.GetConnectionString("beststore")!;
+    options.UseSqlServer(connexionString);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ WebStoreApiEF/Services/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebStoreApiEF.Models;
+
+namespace WebStoreApiEF.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
